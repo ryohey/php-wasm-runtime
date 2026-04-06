5839a323cd394f19e2836b7ef22bb9048144b6d7
ryohey <info@codingcafe.jp>
2026-04-06 19:47:46 +0900

    Trim raw host dispatch overhead

--- packages/runtime/src/RawHostFunc.php
+++ packages/runtime/src/RawHostFunc.php
@@ -6,20 +6,11 @@ namespace WasmRuntime;
 
 final class RawHostFunc
 {
-    private readonly \Closure $handler;
+    public readonly \Closure $handler;
 
     public function __construct(callable $handler)
     {
         $this->handler = \Closure::fromCallable($handler);
     }
 
-    public function invoke(array $stack, int $base, int $count): mixed
-    {
-        return ($this->handler)($stack, $base, $count);
-    }
-
-    public function invokeArgs(array $args): mixed
-    {
-        return ($this->handler)($args, 0, count($args));
-    }
 }
